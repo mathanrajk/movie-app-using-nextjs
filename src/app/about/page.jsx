13120531eb2b990c70d48b44233993a636323d1c
mathanrajk <kmathan301@gmail.com>
2023-08-13 07:57:45 +0530

    movie app with theme

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className=' max-w-6xl mx-auto space-y-4'>
+            <h1 className=' text-2xl font-medium text-amber-600'>
+                About
+            </h1>
+            <div>
+                <p>MDb (an acronym for Internet Movie Database) is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it has been owned and operated by IMDb.com, Inc., a subsidiary of Amazon.</p>
+
+                <p>The site's message boards were disabled in February 2017. As of 2019, IMDb was the 52nd most visited website on the internet, as ranked by Alexa.</p>
+
+                <p>As of March 2022, the database contained some 10.1 million titles (including television episodes), 11.5 million person records, and 83 million registered users.The title and talent pages of IMDb are accessible to all users, but only registered and logged-in users can submit new material and suggest edits to existing entries. Most of the site's data has been provided by these volunteers. Registered users with a proven track record are able to add and make corrections to cast lists, credits, and some other data points. However, the addition and removal of images, and alterations to titles, cast and crew names, character names, and plot summaries are subject to an approval process; this usually takes between 24 and 72 hours.</p>
+
+                <p>On October 2, 2007,[citation needed] character filmographies were added. Character entries are created from character listings in the main filmography database, and as such do not need any additional verification by IMDb staff. They have already been verified when they are added to the main filmography.</p>
+
+                <p>Registered users can choose their username, and most are pseudonymous. There is no single index of contributors, no index on each profile page of the items contributed, and—except for plot synopses and biographies—no identification of contributors to each product's or person's data pages. Users are also invited to rate titles on a scale of 1 to 10, and the totals are converted into a weighted mean-rating, with filters in place to mitigate ballot-stuffing.</p>
+
+                <p>User profile pages show a user's registration date and, optionally, their personal ratings of titles. Since 2015, "badges" can be added showing a count of contributions. These badges range from total contributions made to independent categories such as photos, trivia, and biographies. If a registered user or visitor is in the entertainment industry and has an IMDb page, they can add photos through IMDbPRO.</p></div>
+        </div>
+    )
+}
+
+export default About
